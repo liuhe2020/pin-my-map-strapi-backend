6412a0f697db59227061ee209b9a280a911e2c88
Liu He <125852364@qq.com>
2021-10-02 16:17:07 +0100

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fc8189d273aeecb432b084636219c53f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1d33a2fb-e638-4675-b652-11bc9c104104'
+};
